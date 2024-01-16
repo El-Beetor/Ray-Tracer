56bbf7026886f34f7a31c3d31af70f10cb7aa152
El-Beetor <vhg@ucsb.edu>
2024-01-15 16:29:22 -0800

    added sampling

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -1,10 +1,11 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
+
 #include <cmath>
 #include <limits>
 #include <memory>
-
+#include <cstdlib>
 
 // Usings
 
@@ -23,6 +24,15 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
 // Common Headers
 
 #include "ray.h"
